2be1af23a5e20e73d9403d4297729aee83142ae3
298818 fic piotr <ficp@p20309.mini.pw.edu.pl>
2019-10-04 17:17:25 +0200

    Zadanie 1 laboratoria wykonane 17.15

---
+++ proton/FicPiotr/skrypt.r
@@ -0,0 +1,27 @@
+install.packages("BetaBit")
+library(BetaBit)
+proton()
+head(employees)
+tail(employees)
+employees[employees$surname=='Insecure',]
+proton(action = "login", login="johnins")
+
+head(top1000passwords)
+for (haslo in top1000passwords) {
+  proton(action = "login", login="johnins", password=haslo)
+}
+
+head(logs)
+library(dplyr)
+dplyr::count(hosty)
+employees[employees$surname=='Pietraszko',]
+hosty <- logs[logs$login=='slap',]
+hosty <- group_by(hosty, by = hosty$host)
+dplyr::count(hosty)
+proton(action = "server", host="194.29.178.16")
+
+head(bash_history)
+bash <- bash_history
+unique(sub(' .*', '', bash_history))
+
+proton(action = "login", login="slap", password='DHbb7QXppuHnaXGN')
