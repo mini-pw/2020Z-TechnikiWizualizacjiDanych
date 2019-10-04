994ad05daa8580e75545d5dcdecb8861091905eb
298828 malec mikolaj <malecm@p20315.mini.pw.edu.pl>
2019-10-04 15:37:45 +0200

    proton 1-3

--- proton/mojplik.R
+++ proton/mojplik.R
@@ -1,1 +1,15 @@
-print("succes")
+install.packages("BetaBit")
+library(BetaBit)
+proton()
+#1
+log <- employees$login[employees$name=='John' & employees$surname=='Insecure']
+proton(action = "login", login=log)
+#2
+for( i in 1:1000){
+  proton(action = "login", login=log, password=top1000passwords[i])
+}
+#3
+pLogs<-logs[logs$login==employees$login[employees$surname=='Pietraszko'],'host']
+sort(table(pLogs),decreasing = TRUE)[1]
+proton(action = "server", host="194.29.178.16")
+
