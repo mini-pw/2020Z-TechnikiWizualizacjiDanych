20e4fb45098008ed4bb4823a972681fed57def6a
298845 szypula jakub <szypulaj@p20305.mini.pw.edu.pl>
2019-10-04 19:13:36 +0200

    echhhhh

--- proton/SzypułaJakub/skrypt.R
+++ proton/SzypułaJakub/skrypt.R
@@ -1,6 +1,7 @@
 install.packages("BetaBit")
 library(BetaBit)
 library(dplyr)
+library(stringi)
 proton()
 employees %>% filter(name=="John") %>% head
 employees %>% filter(surname == "Pietraszko")
@@ -10,7 +11,12 @@ while(test == test1) {
   test <- proton(action="login", login="johnins", password=top1000passwords[i])
   i <- i + 1
 }
+i <- 120
 proton(action="login", login="johnins", password=top1000passwords[i])
 logs %>% head()
 mylogs <- logs
 logs %>% filter(login=="slap") %>% group_by(login, host) %>% tally()
+proton(action="server", host="194.29.178.16")
+bash_history%>% head
+bash_history[!grepl(".* ", bash_history)] %>% unique()
+proton(action="login", login="slap", password= "DHbb7QXppuHnaXGN")
