673e7b7b6f5d586e241e50da77d83995cd914842
pawel99k <43797956+pawel99k@users.noreply.github.com>
2019-11-02 19:30:13 +0100

    Update scales-coordinates-multiplots.R

--- Materiały/TYDZIEN4/scales-coordinates-multiplots.R
+++ Materiały/TYDZIEN4/scales-coordinates-multiplots.R
@@ -2,6 +2,10 @@ library(ggplot2)
 library(SmarterPoland)
 library(dplyr)
 
+ggplot(countries, aes(x = continent, label = paste(continent, 2, sep = "\n"))) +
+  geom_bar() +
+  geom_text(stat = "count")
+
 # 1. skale -----------------------------------------------------------------
 
 p <- ggplot(data = countries, aes(x = continent, fill = continent)) +
@@ -24,6 +28,12 @@ continent_order <- group_by(countries, continent) %>%
 
 p + scale_x_discrete(limits = continent_order)
 
+mutate(countries, 
+       country = factor(country, levels = countries[order(countries[["birth.rate"]]), "country"])) %>% 
+  filter(continent == "Europe") %>% 
+  ggplot(aes(x = country, y = birth.rate)) +
+  geom_col()
+
 countries_f <- mutate(countries, continent = factor(continent))
 
 ggplot(data = countries_f, aes(x = continent, fill = continent)) +
@@ -34,7 +44,8 @@ mutate(countries_f, continent = factor(continent, levels = continent_order)) %>%
   ggplot(aes(x = continent, fill = continent)) +
   geom_bar()
 
-ggplot(countries_f, aes(x = death.rate, y = birth.rate)) +
+mutate(countries_f, continent = factor(continent, levels = continent_order)) %>% 
+  ggplot(aes(x = death.rate, y = birth.rate)) +
   geom_point() +
   facet_wrap(~ continent)
 
@@ -51,11 +62,12 @@ cntr_pop <- group_by(countries, continent) %>%
   mutate(both = paste0(continent, " (", count, " countries)")) %>% 
   pull(both)
 
-mutate(countries_f, continent = factor(continent, levels = continent_order, labels = cntr_pop)) %>% 
+mutate(countries_f, continent = factor(continent, 
+                                       levels = continent_order, 
+                                       labels = cntr_pop)) %>% 
   ggplot(aes(x = death.rate, y = birth.rate)) +
   geom_point() +
   facet_wrap(~ continent)
-
 
 mutate(countries_f, continent = factor(continent, levels = sapply(strsplit(cntr_pop, " "), first), 
                                        labels = cntr_pop)) %>% 
@@ -66,12 +78,20 @@ mutate(countries_f, continent = factor(continent, levels = sapply(strsplit(cntr_
 # color brewer: http://colorbrewer2.org/#type=sequential&scheme=BuGn&n=3
 # alternatywnie library(RColorBrewer)
 
+p + scale_fill_manual(values = c("red", "grey", "black", "navyblue", "green"))
+
 p + scale_fill_manual(values = rainbow(5))
+
 # gradienty: przykladowo scale_fill_gradient()
 
 ggplot(countries, aes(x = birth.rate, y = death.rate)) +
   stat_density_2d(aes(fill = ..level..), color = "black", contour = TRUE, geom = "polygon") +
   scale_fill_gradient(low = "navyblue", high = "red")
+
+ggplot(countries, aes(x = birth.rate, y = death.rate)) +
+  stat_density_2d(aes(fill = ..level..), color = "black", contour = TRUE, geom = "polygon") +
+  scale_fill_gradient2(low = "navyblue", high = "red", mid = "white", 
+                       midpoint = 0.004)
 
 p + theme(legend.position = "bottom") +
   guides(fill = guide_legend(nrow = 2))
@@ -85,8 +105,11 @@ p + coord_flip()
 
 p + coord_flip() + scale_y_reverse()
 
-
 p + coord_polar()
+
+ggplot(data = countries, aes(x = 1, fill = continent)) +
+  geom_bar(position = "stack") +
+  coord_polar()
 
 ggplot(countries, aes(x = death.rate, y = birth.rate)) +
   geom_point() +
@@ -97,11 +120,11 @@ p <- ggplot(data = countries, aes(x = birth.rate, y = death.rate, color = contin
   geom_point() +
   geom_smooth(se = FALSE)
 
-p + coord_equal()
+p + coord_equal() + facet_wrap(~ continent)
 
 # coord_cartesian nie usuwa punktów
 p + coord_cartesian(xlim = c(5, 15))
-p + scale_x_continuous(limits = c(5, 15))
+p + scale_x_continuous(limits = c(5, 15)) 
 
 # 3. wiele wykresow na jednym rysunku ---------------------------------------------------
 
@@ -109,6 +132,18 @@ library(gridExtra)
 
 grid.arrange(p + coord_cartesian(xlim = c(5, 10)) + ggtitle("coord_cartesian"),
              p + scale_x_continuous(limits = c(5, 10)) + ggtitle("scale_x_continuous - limits"),
+             ncol = 1)
+
+grid.arrange(p + coord_cartesian(xlim = c(5, 10)) + ggtitle("coord_cartesian") + 
+               theme(legend.position = "none"),
+             p + scale_x_continuous(limits = c(5, 10)) + ggtitle("scale_x_continuous - limits"),
+             ncol = 1)
+
+grid.arrange(p + coord_cartesian(xlim = c(5, 10)) + ggtitle("coord_cartesian") + 
+               theme(legend.position = "none"),
+             p + scale_x_continuous(limits = c(5, 10)) + 
+               ggtitle("scale_x_continuous - limits") +
+               theme(legend.position = "bottom"),
              ncol = 1)
 
 main_plot <- ggplot(data = countries, aes(x = birth.rate, y = death.rate, color = continent)) +
@@ -125,10 +160,13 @@ density_birth <- ggplot(data = countries, aes(x = birth.rate, fill = continent))
   scale_y_reverse() +
   theme(legend.position = "none")
 
-library(gridExtra)
+grid.arrange(density_death, main_plot, density_birth, 
+             ncol = 2)
+
 library(grid)
 
-grid.arrange(density_death, main_plot, density_birth, 
+
+grid.arrange(density_death, main_plot, rectGrob(gp = gpar(fill = NA, col = NA)), density_birth, 
              ncol = 2)
 
 grid.arrange(density_death, main_plot, rectGrob(gp = gpar(fill = NA, col = NA)), density_birth, 
@@ -142,6 +180,9 @@ get_legend <- function(gg_plot) {
   grob_table[["grobs"]][[which(sapply(grob_table[["grobs"]], function(x) x[["name"]]) == "guide-box")]]
 }
 
+grid.arrange(density_death, main_plot + theme(legend.position = "none"), 
+             get_legend(main_plot), density_birth, 
+             ncol = 2, heights = c(0.7, 0.3), widths = c(0.3, 0.7))
 
 
 main_plot <- ggplot(data = countries, aes(x = birth.rate, y = death.rate, color = continent)) +
@@ -195,24 +236,3 @@ p1 / p2
 # rozklady brzegowe w patchwork
 density_death + main_plot + plot_spacer() + density_birth + 
   plot_layout(ncol = 2, heights = c(0.7, 0.3), widths = c(0.3, 0.7))
-
-plot1 <- countries %>%
-  group_by(continent) %>%
-  summarise(srednia = mean(birth.rate, na.rm = TRUE)) %>%
-  ggplot(aes(x = continent, y = srednia)) +
-  geom_col(aes(x = continent, y = srednia), fill = c('#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3')) +
-  coord_flip() +
-  scale_y_reverse()
-
-plot2 <- countries %>%
-  group_by(continent) %>%
-  summarise(srednia = mean(death.rate, na.rm = TRUE)) %>%
-  ggplot(aes(x = continent, y = srednia)) +
-  geom_col(aes(x = continent, y = srednia), fill = c('#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3')) +
-  coord_flip() +
-  theme(axis.title.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.text.y = element_blank())
-  
-plot1 + plot2
-  
