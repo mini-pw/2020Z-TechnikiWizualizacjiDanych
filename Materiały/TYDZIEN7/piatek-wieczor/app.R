908c434955310eccd46cc3e7587d59c3d3d13c02
michbur <michalburdukiewicz@gmail.com>
2019-11-22 18:30:48 +0100

    added checkbox

--- Materiały/TYDZIEN7/piatek-wieczor/app.R
+++ Materiały/TYDZIEN7/piatek-wieczor/app.R
@@ -1,10 +1,14 @@
 library(shiny)
 library(ggplot2)
 library(SmarterPoland)
+library(dplyr)
 
 ui <- fluidPage(
   title = "Piątek wieczór",
   plotOutput(outputId = "countries_plot", click = "countries_click"),
+  checkboxGroupInput(inputId = "countries_choose", label = "Wybierz kontynent", 
+                     choices = unique(countries[["continent"]]),
+                     selected = unique(countries[["continent"]])),
   verbatimTextOutput(outputId = "click_res")
 )
 
@@ -12,14 +16,15 @@ server <- function(input, output) {
   
   output[["countries_plot"]] <- renderPlot({
     
-    ggplot(countries, aes(x = birth.rate, y = death.rate, color = continent)) +
+    filter(countries, continent %in% input[["countries_choose"]]) %>% 
+      ggplot(aes(x = birth.rate, y = death.rate, color = continent)) +
       geom_point()
-  
-    })
+    
+  })
   
   output[["click_res"]] <- renderPrint({
     
-    input[["countries_click"]]
+    nearPoints(countries, input[["countries_click"]])
     
   })
   
