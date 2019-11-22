f78be782f7d800de1afc673ebac06326ce62eb16
michbur <michalburdukiewicz@gmail.com>
2019-11-22 16:35:30 +0100

    added select axis

--- Materiały/TYDZIEN7/python/app.R
+++ Materiały/TYDZIEN7/python/app.R
@@ -1,15 +1,35 @@
 library(SmarterPoland)
 library(shiny)
 library(ggplot2)
+library(ggstance)
 
 ui <- fluidPage(title = "Aplikacja python",
-                plotOutput(outputId = "countries_plot"))
+                plotOutput(outputId = "countries_plot"),
+                selectInput(inputId = "x_select", label = "Wybierz os x", 
+                            choices = colnames(countries), selected = "birth.rate"),
+                selectInput(inputId = "y_select", label = "Wybierz os y", 
+                            choices = colnames(countries), selected = "death.rate"),
+                selectInput(inputId = "cf_select", label = "Wybierz kolor", 
+                            choices = colnames(countries), selected = "continent"))
 
 server <- function(input, output) {
   
   output[["countries_plot"]] <- renderPlot({
-    ggplot(countries, aes(x = birth.rate, y = death.rate, color = continent)) +
-      geom_point() +
+    chosen_geom <- if(is.character(countries[[input[["x_select"]]]])) {
+      geom_boxplot()
+    } else {
+      if(is.character(countries[[input[["y_select"]]]])) {
+        geom_boxploth()
+      } else {
+        geom_point()
+      }
+    }
+    
+    #chosen_coords <- if()
+    
+    ggplot(countries, aes_string(x = input[["x_select"]], y = input[["y_select"]], 
+                                 color = input[["cf_select"]])) +
+      chosen_geom +
       theme_bw() +
       theme(legend.position = "bottom")
   })
