6fdfd604d1d739179e397d2bb4871a0187af0eb3
michbur <michalburdukiewicz@gmail.com>
2019-11-22 16:56:18 +0100

    added themes and sidebar

--- Materiały/TYDZIEN7/python/app.R
+++ Materiały/TYDZIEN7/python/app.R
@@ -3,17 +3,20 @@ library(shiny)
 library(ggplot2)
 library(ggstance)
 library(plotly)
+library(shinythemes)
 
 ui <- fluidPage(title = "Aplikacja python",
-                plotOutput(outputId = "countries_plot", hover = "countries_hover"),
-                selectInput(inputId = "x_select", label = "Wybierz os x", 
-                            choices = colnames(countries), selected = "birth.rate"),
-                selectInput(inputId = "y_select", label = "Wybierz os y", 
-                            choices = colnames(countries), selected = "death.rate"),
-                selectInput(inputId = "cf_select", label = "Wybierz kolor", 
-                            choices = colnames(countries), selected = "continent"),
-                verbatimTextOutput(outputId = "hover_result"),
-                plotlyOutput(outputId = "countries_plotly"))
+                theme = shinytheme("cyborg"),
+                sidebarPanel(selectInput(inputId = "x_select", label = "Wybierz os x", 
+                                         choices = colnames(countries), selected = "birth.rate"),
+                             selectInput(inputId = "y_select", label = "Wybierz os y", 
+                                         choices = colnames(countries), selected = "death.rate"),
+                             selectInput(inputId = "cf_select", label = "Wybierz kolor", 
+                                         choices = colnames(countries), selected = "continent")),
+                mainPanel(plotOutput(outputId = "countries_plot", hover = "countries_hover"),
+                          verbatimTextOutput(outputId = "hover_result"),
+                          plotlyOutput(outputId = "countries_plotly"))
+)
 
 server <- function(input, output) {
   
