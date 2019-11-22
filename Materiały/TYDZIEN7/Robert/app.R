97861605a820e25f708b853f0207f33314871915
michbur <michalburdukiewicz@gmail.com>
2019-11-22 14:47:55 +0100

    added checkboxGroupInput

--- Materiały/TYDZIEN7/Robert/app.R
+++ Materiały/TYDZIEN7/Robert/app.R
@@ -4,13 +4,17 @@ library(ggplot2)
 library(dplyr)
 
 ui <- fluidPage(
-  plotOutput(outputId = "countries_plot")
+  plotOutput(outputId = "countries_plot"),
+  checkboxGroupInput(inputId = "countries_choose", label = "Wybierz kontynenty", 
+                     choices = unique(countries[["continent"]]))
 )
 
 server <- function(input, output) {
   
   output[["countries_plot"]] <- renderPlot({
-    ggplot(countries, aes(x = birth.rate, y = death.rate, color = continent)) +
+    countries %>% 
+      filter(continent %in% input[["countries_choose"]]) %>% 
+      ggplot(aes(x = birth.rate, y = death.rate, color = continent)) +
       geom_point()
   })
   
