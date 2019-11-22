53a53c084003295ccc61b2a9c97287cb00546bb9
michbur <michalburdukiewicz@gmail.com>
2019-11-22 14:55:50 +0100

    added palette

--- Materiały/TYDZIEN7/Robert/app.R
+++ Materiały/TYDZIEN7/Robert/app.R
@@ -6,16 +6,22 @@ library(dplyr)
 ui <- fluidPage(
   plotOutput(outputId = "countries_plot"),
   checkboxGroupInput(inputId = "countries_choose", label = "Wybierz kontynenty", 
-                     choices = unique(countries[["continent"]]))
+                     choices = unique(countries[["continent"]]),
+                     selected = unique(countries[["continent"]]))
 )
 
 server <- function(input, output) {
   
   output[["countries_plot"]] <- renderPlot({
+    unique_continents <- unique(countries[["continent"]])
+    lazy_palette <- rainbow(length(unique_continents)) %>% 
+      setNames(unique_continents)
+    
     countries %>% 
       filter(continent %in% input[["countries_choose"]]) %>% 
       ggplot(aes(x = birth.rate, y = death.rate, color = continent)) +
-      geom_point()
+      geom_point() +
+      scale_color_manual(values = lazy_palette)
   })
   
 }
