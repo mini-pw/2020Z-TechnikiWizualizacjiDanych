bbcd1da240396eec2412b6d896037a3651c1ced4
michbur <michalburdukiewicz@gmail.com>
2019-11-29 14:41:41 +0100

    added observeEvent

--- Materiały/TYDZIEN8/SimpleObserverGr1/app.R
+++ Materiały/TYDZIEN8/SimpleObserverGr1/app.R
@@ -8,25 +8,30 @@ ui <- fluidPage(
   verbatimTextOutput("processed_selection"),
   plotOutput("countries_plot", height = 600, 
              click = "countries_click")
-
+  
 )
 
 server <- function(input, output) {
   
-  selected_country <- reactive({
-    nearPoints(countries, input[["countries_click"]], 
-               maxpoints = 1)
+  selected_country <- reactiveValues(
+    country = character()
+  )
+  
+  observeEvent(input[["countries_click"]], {
+    selected_country[["country"]] <- nearPoints(countries, input[["countries_click"]],
+                                                maxpoints = 1)[["country"]]
   })
   
+  
   output[["countries_plot"]] <- renderPlot({
-    mutate(countries, selected = country %in% selected_country()[["country"]]) %>% 
+    mutate(countries, selected = country %in% selected_country[["country"]]) %>% 
       ggplot(aes(x = birth.rate, y = death.rate, color = continent, size = selected)) +
       geom_point() +
       theme_bw()
   })
   
   output[["processed_selection"]] <- renderPrint({
-    selected_country()
+    selected_country[["country"]]
   })
   
 }
