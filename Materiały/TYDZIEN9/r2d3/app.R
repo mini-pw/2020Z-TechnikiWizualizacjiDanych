abe5d7cac9378e44b6279b3f8c746ae9e3cac3b8
michbur <michalburdukiewicz@gmail.com>
2019-12-06 16:34:34 +0100

    updated app

--- Materiały/TYDZIEN9/r2d3/app.R
+++ Materiały/TYDZIEN9/r2d3/app.R
@@ -4,14 +4,14 @@ library(r2d3)
 ui <- fluidPage(
   inputPanel(
     sliderInput("bar_max", label = "Max:",
-                min = 0, max = 15, value = 5, step = 1)
+                min = 0, max = 1, value = 1, step = 0.05)
   ),
   d3Output("d3")
 )
 
 server <- function(input, output) {
   output[["d3"]] <- renderD3({
-    r2d3(runif(n = input[["bar_max"]], min = 0, max = 5), 
+    r2d3(runif(n = 5, min = 0, max = input[["bar_max"]]), 
          script = "baranims.js"
     )
   })
