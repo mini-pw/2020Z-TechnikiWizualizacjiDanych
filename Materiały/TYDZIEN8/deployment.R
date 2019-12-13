fc8d11998de09b0178d88e96e23c965d646db927
PlentyCoups <matis33pl@gmail.com>
2019-12-13 13:18:52 +0100

    commit

--- Materiały/TYDZIEN8/deployment.R
+++ Materiały/TYDZIEN8/deployment.R
@@ -1,3 +1,6 @@
-# install.packages("rsconnect")
+  # install.packages("rsconnect")
 # token: https://www.shinyapps.io/
+rsconnect::setAccountInfo(name='plentycoups',
+                          token='F03F1AE32FC36F11A51A0BB76B07BBEB',
+                          secret='lNmNNL6jpVYWB1jVmcInHKuAj6Hy55jaPu1KjWxW')
 rsconnect::deployApp(appDir = "Materiały/TYDZIEN8/SimpleObserver/")
---
+++ PraceDomowe/TYDZIEN8/GrzybMateusz/app.R
@@ -0,0 +1,20 @@
+library(shiny)
+library(r2d3)
+
+ui <- fluidPage(
+  inputPanel(
+    sliderInput("bar_max", label = "Max:",
+                min = 0, max = 15, value = 5, step = 1)
+  ),
+  d3Output("d3")
+)
+
+server <- function(input, output) {
+  output[["d3"]] <- renderD3({
+    r2d3(runif(n = input[["bar_max"]], min = 0, max = 5), 
+         script = "tree.js"
+    )
+  })
+}
+
+shinyApp(ui = ui, server = server)
